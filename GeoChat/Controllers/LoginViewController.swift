7df96803aaf03083e6e60e91f10f46711ea2e625
Nikolai Saganenko <9952728@gmail.com>
2021-11-01 10:55:37 +0300

    Add Registration and Login ViewControllers

---
+++ GeoChat/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  GeoChat
+//
+//  Created by Nick Sagan on 01.11.2021.
+//
+
+import UIKit
+//import Firebase
+
+class LoginViewController: UIViewController {
+
+//    @IBOutlet weak var emailTextfield: UITextField!
+//    @IBOutlet weak var passwordTextfield: UITextField!
+//
+//
+//    @IBAction func loginPressed(_ sender: UIButton) {
+//
+//        if let email = emailTextfield.text, let password = passwordTextfield.text{
+//            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+//                if let e = error{
+//                    print(e)
+//                } else{
+//                    self?.performSegue(withIdentifier: K.loginSegue, sender: self)
+//                }
+//            }
+//        }
+//    }
+    
+}
---
+++ GeoChat/Controllers/RegistrationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationViewController.swift
+//  GeoChat
+//
+//  Created by Nick Sagan on 01.11.2021.
+//
+
+import UIKit
+//import Firebase
+
+class RegistrationViewController: UIViewController {
+
+    
+//    @IBAction func registrationPressed(_ sender: UIButton) {
+//
+//        if let email = emailTextfield.text, let password = passwordTextfield.text{
+//        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+//            if let e = error{
+//                print(e)
+//                self.errorLabel.text = e.localizedDescription
+//            }
+//            else{
+//                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+//            }
+//            }
+//        }
+//    }
+    
+}
